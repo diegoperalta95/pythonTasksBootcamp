0dbb515380182dae939eac0900a6a9442f65a4ab
Diego Peralta <diegooperalta95@gmail.com>
2023-11-08 18:43:17 -0300

    Menu basico

---
+++ agenda.py
@@ -0,0 +1,8 @@
+menu = True
+while menu:
+    print ('1 - Create a new contact')
+    print ('2 - Search')
+    print ('3 - Help')
+    print ('4 - Exit')
+    menuOption = input ('Ingrese una opción: ')
+
